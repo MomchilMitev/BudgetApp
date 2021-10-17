e292edef5c31ae9116a85c230eb7692bd3dabeeb
momchilmitev <momchilmitev87@gmail.com>
2021-10-17 20:27:41 +0200

    added basic file structure and logic

---
+++ views/transactions.php
@@ -0,0 +1,59 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>BudgetApp</title>
+    <style>
+        table {
+            width: 100%;
+            border-collapse: collapse;
+            text-align: center;
+        }
+
+        table tr th, table tr td {
+            padding: 5px;
+            border: 1px #eee solid;
+        }
+
+        tfoot tr th, tfoot tr td {
+            font-size: 20px;
+        }
+
+        tfoot tr th {
+            text-align: right;
+        }
+    </style>
+</head>
+<body>
+    <table>
+        <thead>
+            <tr>
+                <th>Date</th>
+                <th>Check</th>
+                <th>Description</th>
+                <th>Amount</th>
+            </tr>
+        </thead>
+        <tbody>
+
+        </tbody>
+        <tfoot>
+            <tr>
+                <th colspan="3">Total Income:</th>
+                <td></td>
+            </tr>
+            <tr>
+                <th>Total Expense:</th>
+                <td></td>
+            </tr>
+            <tr>
+                <th>Net Total:</th>
+                <td></td>
+            </tr>
+        </tfoot>
+    </table>
+</body>
+</html>
