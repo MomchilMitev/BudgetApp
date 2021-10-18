dd2c3d580baac5351fcedb8a866b13ee9a21ccbc
momchilmitev <momchilmitev87@gmail.com>
2021-10-18 09:49:12 +0200

    added getTransactionFiles and getTransactions functions

--- app/App.php
+++ app/App.php
@@ -1,3 +1,35 @@
 <?php
 
 declare(strict_types=1);
+
+function getTransactionFiles(string $dirPath): array {
+    $files = [];
+
+    foreach (scandir($dirPath) as $file) {
+        if (is_dir($file)) {
+            continue;
+        }
+
+        $files[] = $dirPath . $file;
+    }
+
+    return $files;
+}
+
+function getTransactions(string $filePath): array {
+    if (! file_exists($filePath)) {
+        trigger_error('File "' . $filePath . '" does not exist!', E_USER_ERROR);
+    }
+
+    $file = fopen($filePath, 'r');
+
+    fgetcsv($file);
+
+    $transactions = [];
+
+    while (($transaction = fgetcsv($file)) !== false) {
+        $transactions[] = $transaction;
+    }
+
+    return $transactions;
+}
