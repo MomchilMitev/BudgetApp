829ade8be123c69bf7a7dba3946cb7171e7a6403
momchilmitev <momchilmitev87@gmail.com>
2022-06-25 20:10:25 +0200

    added the basic counter functionality

--- main.js
+++ main.js
@@ -1,1 +1,57 @@
 import './css/main.css'
+
+const pomodoroMinutes = 20
+let interval = null;
+let remainingSeconds = pomodoroMinutes * 60;
+const pomodoroCounter = document.querySelector('.pomodoro__counter')
+const pomodoroTime = document.querySelector('.pomodoro__minutes')
+const pomodoroAction = document.querySelector('.pomodoro__action')
+
+pomodoroCounter.addEventListener('click', e => {
+  if (interval === null) {
+    start();
+  } else {
+    stop();
+  }
+})
+
+function start() {
+  if (remainingSeconds === 0) return;
+
+  interval = setInterval(() => {
+    remainingSeconds--;
+    updateTime();
+
+    if (remainingSeconds === 0) {
+      stop();
+    }
+  }, 1000);
+
+  updateText();
+}
+
+function stop() {
+  clearInterval(interval);
+
+  interval = null;
+
+  updateText();
+}
+
+function updateTime() {
+  const minutes = (Math.floor(remainingSeconds / 60)).toString().padStart(2, "0");
+  const seconds = (remainingSeconds % 60).toString().padStart(2, "0");
+  pomodoroTime.textContent = `${minutes}:${seconds}`
+}
+
+function updateText() {
+  if (interval === null) {
+    pomodoroAction.textContent = 'start'
+  } else {
+    pomodoroAction.textContent = 'pouse'
+  }
+}
+
+window.onload = () => {
+  start()
+}
