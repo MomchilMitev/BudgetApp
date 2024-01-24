0135385de896e18aaa75cff91ca0b2e6dfef73ea
momchilmitev <momchilmitev87@gmail.com>
2024-01-24 20:48:28 +0100

    added the collor palette and body bg color

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,16 @@
 export default {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
+    colors: {
+      red: '#F87070',
+      blue: '#70F3F8',
+      lila: '#D881F8',
+      'lila-ligth': '#D7E0FF',
+      'black-light': '#1E213F',
+      white: '#FFFFFF',
+      pink: '#EFF1FA',
+      'black-dark': '#161932'
+    },
     extend: {
       fontFamily: {
         kumbh: ['Kumbh Sans', 'sans-serif'],
