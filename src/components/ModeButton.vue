6efd52ed76b8cdd1f4de1e285ffc0d607f4506fb
momchilmitev <momchilmitev87@gmail.com>
2024-01-24 21:57:04 +0100

    added styles for active and focus state on the mode btns

--- src/components/ModeButton.vue
+++ src/components/ModeButton.vue
@@ -6,7 +6,23 @@
 
 <template>
   <button
-    class="bg-transparent border-0 rounded-[26.5px] cursor-pointer text-lila-light px-6 py-4 transition-all duration-300"
+    class="
+      active:bg-red 
+      active:text-black-light 
+      focus:bg-red 
+      focus:text-black-light
+      focus:outline-none
+      bg-transparent 
+      border-0 
+      rounded-[26.5px] 
+      cursor-pointer 
+      text-sm 
+      text-lila-light 
+      px-6 
+      py-4 
+      transition-all 
+      duration-300
+    "
   >
     {{ text }}
   </button>
