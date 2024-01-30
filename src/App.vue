8a502ad26a0af77beed480c467c4a226db56f3f6
momchilmitev <momchilmitev87@gmail.com>
2024-01-30 21:28:00 +0100

    added the settings button component

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
-  import { ButtonsWrapper, TimerWrapper, HeaderLogo } from "@/components";
+  import { ButtonsWrapper, SettingsButton, TimerWrapper, HeaderLogo } from "@/components";
 </script>
 
 <template>
   <HeaderLogo />
   <ButtonsWrapper />
   <TimerWrapper />
+  <SettingsButton />
 </template>
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,9 +1,11 @@
 import ButtonsWrapper from "./ButtonsWrapper.vue";
 import HeaderLogo from "./HeaderLogo.vue";
+import SettingsButton from "./SettingsButton.vue";
 import TimerWrapper from '@/components/TimerWrapper.vue'
 
 export {
   ButtonsWrapper,
   HeaderLogo,
+  SettingsButton,
   TimerWrapper,
 }
